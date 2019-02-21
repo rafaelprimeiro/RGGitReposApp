d1a3d9fd3f903f34d66ec50762d99dd348d331f2
Rafael Gabriel <rafael.gabriel@itlean.com.br>
2019-02-21 00:43:06 -0300

    Teste unitário

--- RGGithubAppTests/RGGithubAppTests.swift
+++ RGGithubAppTests/RGGithubAppTests.swift
@@ -10,25 +10,31 @@ import XCTest
 @testable import RGGithubApp
 
 class RGGithubAppTests: XCTestCase {
+  
+  let service = try! GithubService(baseURL: "https://api.github.com")
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  override func setUp() {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+  func testRequestRepos() {
+    self.measure {
+      service.fetchRepos() { githubResult in
+        guard let githubResult = githubResult else {
+          XCTAssertTrue(false, "githubResult retornou null.")
+          return
         }
+        guard let _ = githubResult.items else {
+          XCTAssertTrue(false, "Não retornou itens. Talvez tenha chegado no limite de request por minuto.")
+          return
+        }
+        XCTAssertTrue(true)
+      }
     }
+  }
 
 }
